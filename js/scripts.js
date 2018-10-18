e717c80d8b798bd1ae41e02b025170c288c9ccda
Azul Rodriguez Da Silva and Jentry J <jentry@>
2018-10-17 20:13:32 -0700

    add fully functional algorithm to display a users fortune based on refults, started adding a restart button

--- js/scripts.js
+++ js/scripts.js
@@ -32,13 +32,43 @@ $(document).ready(function(){
 
 
     if  (total === 0) {
-      $("#results").append(
+      $("#answer").append(
         '<h4>Your Fortune Is...</h4>' +
-        '<h1>This is the calm before the storm</h1>');
-    };
-
-    $("#results").show();
-
+        '<h3>This is the calm before the storm</h3>');
+        $("#results").show();
+      } else if (total > 3) {
+        $("#answer").append(
+          '<h4>Your Fortune Is...</h4>' +
+          '<h3>Go buy lottery ticket and ask out Rihanna because you\'re fire.</h3>');
+          $("#results").show();
+      } else if (total <= 3 && total > 0) {
+        $("#answer").append(
+          '<h4>Your Fortune Is...</h4>' +
+          '<h3>Go ask Rihanna out but don’t expect any response. <br> Luckily you will also notice your shoe is untied and not trip AT ALL.</h3>');
+          $("#results").show();
+      } else if (total < 0 && total > -4) {
+        $("#answer").append(
+          '<h4>Your Fortune Is...</h4>' +
+          '<h3>Keep an eye out for your shoe laces getting untied. You’ll probably forget, and trip a little.</h3>');
+          $("#results").show();
+      } else if (total < -3) {
+        $("#answer").append(
+          '<h4>Your Fortune Is...</h4>' +
+          '<h3>Just give up, I’m sorry. <br>Try again next week after binge-watching Brooklyn 99 for some levity.</h3>');
+          $("#results").show();
+      } else {
+        alert("Pick some answers, you dangoose!");
+        $("#quiz").show();
+      };
   });
 });
 //ui logic ends
+
+// ––––This code is attempting to display a message if the user didn't select anything –––––––––
+
+
+// // if {
+// //   $("#quiz:checkbox:checked").prop(false);
+// //   alert("Pick some answers, you dangoose!");
+// //   $("#quiz").show();
+// } else if {
